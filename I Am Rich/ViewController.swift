861616e32523ff3eb1557884616fe138c844462c
Ezzat Qupty <ezzat.qupty12345@gmail.com>
2020-02-07 16:40:21 +0200

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Ezzat Qupty on 07/02/2020.
+//  Copyright © 2020 Ezzat Qupty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
